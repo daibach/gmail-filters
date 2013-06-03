9d83f73dd97299b9329a2d2cf0bd00c7c62f82b9
Dafydd Vaughan <dafydd.vaughan@digital.cabinet-office.gov.uk>
2013-06-03 16:31:27 +0100

    Convert notification filters to use britta

---
+++ notifications-filters.rb
@@ -0,0 +1,108 @@
+#!/bin/env ruby
+
+require 'rubygems'
+require 'gmail-britta'
+
+if File.exist?(File.expand_path("~/.my-email-addresses.rb"))
+  require "~/.my-email-addresses.rb"
+else
+  # Some fake constants to let you run this (-:
+  MY_EMAILS = %w[test@example.com test.tester@example.com]
+end
+
+fs = GmailBritta.filterset(:me => MY_EMAILS) do
+  # Pivotal Filters
+  filter {
+    has %w{from:postmaster.twitter.com}
+    label 'deletable/twitter'
+  }
+  filter {
+    has %w{from:facebookmail.com}
+    label 'deletable/facebook-linkedin'
+  }
+  filter {
+    has %w{from:"linkedin.com OR em.linkedin.com OR e.linkedin.com"}
+    label 'deletable/facebook-linkedin'
+  }
+  filter {
+    has %w{from:wordpress.com}
+    label 'deletable/wordpress'
+  }
+  filter {
+    has %w{from:foursquare.com}
+    label 'deletable/other-social'
+  }
+  filter {
+    has %w{from:untappd.com}
+    label 'deletable/other-social'
+  }
+  #chromaroma
+  filter {
+    has %w{from:slidesharemail.com}
+    label 'deletable/other-social'
+  }
+  filter {
+    has %w{from:flickr.com}
+    label 'deletable/other-social'
+  }
+  filter {
+    has %w{from:toby.barnes@wearemudlark.com "Chromaroma"}
+    label 'deletable/other-social'
+  }
+  filter {
+    has %w{from:alerts@lanyrd.com}
+    label 'deltable/other-social'
+  }
+  filter {
+    has %w{"added you on Google+"}
+    label 'deletable/other-social'
+  }
+  filter {
+    has %w{from:calendar-notification@google.com}
+    label 'deletable/calendar'
+  }
+  filter {
+    has %w{"Invitation from Google Calendar"}
+    label 'deletable/calendar'
+  }
+  filter {
+    has %w{from:today@sunrise.im}
+    label 'deletable/sunrise'
+  }
+  filter {
+    has %w{from:paypal@e.paypal.co.uk}
+    label 'deletable/other-notifications'
+  }
+  filter {
+    has %w{from:gis@monmouthshire.gov.uk}
+    label 'deletable/other-notifications'
+  }
+  filter {
+    has %w{from:updates.hungryhouse.co.uk}
+    label 'deletable/other-notifications'
+  }
+  filter {
+    has %w{from:info.tfl.gov.uk}
+    label 'deletable/other-notifications'
+  }
+  filter {
+    has %w{from:newrelic.com}
+    label 'deletable/other-notifications'
+  }
+  filter {
+    has %w{from:dropbox.com}
+    label 'deletable/other-notifications'
+  }
+  filter {
+    has %w{from:trello.com}
+    label 'deletable/other-notifications'
+  }
+  filter {
+    has %w{subject:"Change Request" from:nobody@google.com}
+    label 'deletable/other-notifications'
+  }
+
+
+end
+
+puts fs.generate
